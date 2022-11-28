ef6fce051217c90e4e649bd679ab6e2c8557471b
NathDCoder <themusicemperor@gmail.com>
2022-11-28 14:26:05 -0500

    gardenhose

---
+++ gardenhose/server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    songs: [Music]!
+  }
+
+  type Music {
+    _id: ID
+    songText: String
+    songAuthor: String
+    createdAt: String
+    comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    songs(username: String): [Music]
+    song(songId: ID!): Music
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addComment(songId: ID!, commentText: String!): Music
+    removeComment(songId: ID!, commentId: ID!): Music
+  }
+`;
+
+module.exports = typeDefs;
