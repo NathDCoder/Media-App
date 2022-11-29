a1a16cb7be1e3ca8f6229c9a6b04036f66652edd
NathDCoder <themusicemperor@gmail.com>
2022-11-28 19:14:34 -0500

    pickaxe

--- gardenhose/src/App.js
+++ gardenhose/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import video from "./Assets/particlewaves.mp4"
+import Home from "./Pages/Home"
+
 
 function App() {
   return (
-    <main>
-      <video src={video} autoPlay loop muted 
-      className="absolute z-10 w-auto 
-      min-w-full min-h-full max-w-none"/>
-    </main>
+    <Home/>
   );
 }
 
