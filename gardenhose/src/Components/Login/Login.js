b3cf9955cb68948182c74cd2bf602bbc7921d7ea
Myson Howard <mysonhoward@Mysons-Air.attlocal.net>
2022-11-28 22:16:02 -0500

    login js

--- gardenhose/src/Components/Login/Login.js
+++ gardenhose/src/Components/Login/Login.js
@@ -3,6 +3,12 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <form>
+      <label for 'email'> Email </label>
+      <input type = 'email' placeholder = 'Enter Email' id = 'email' />
+      <label for 'password'> Password </label>
+      <input type = 'password' placeholder = '' id = 'password' />
+      <button> Log in </button>
+    </form>
   )
 }
