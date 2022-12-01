346b62d7e58c46526ed9948e2d4190aa22c41ed1
NathDCoder <themusicemperor@gmail.com>
2022-12-01 11:02:17 -0500

    Auger

--- gardenhose/src/App.js
+++ gardenhose/src/App.js
@@ -20,6 +20,7 @@ function App() {
     <nav>
       <NavLinks />
       <Title />
+      {/* <Searchbar /> */}
      <Background />
     </nav>
   
--- gardenhose/src/Pages/Genre.js
+++ gardenhose/src/Pages/Genre.js
@@ -5,11 +5,11 @@ import {Genres} from "../Components/Genres/GenresPage";
 function Genre () {
    <Genres/>
    return (
-    <div className="flex justify-center">
-      <div className= "w-full flex relative justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+    <div className="flex relative justify-center">
+      <div className= "flex absolute justify-between items-center sm:flex-row flex-col mt-4 mb-10">
          <h2 className="genrepick font-bold text-3x1 text-white text-left absolute flex justify-center z-30">Pick a Genre</h2>
          <select onChange={() => {}}
-          value="" className ="genre absolute flex justify-center z-30 bg-black text-white p-3 text-sm rounded-lg">
+          className ="genre absolute flex justify-center z-30 bg-black text-white p-3 text-sm rounded-lg">
          {Genres.map((Genres)=> <option key={Genres.value}value={Genres.value}>{Genres.title}</option>)}
          </select>
       </div>
