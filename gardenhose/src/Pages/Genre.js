362c95e47b980b602953cfc2dd311156585a2475
NathDCoder <themusicemperor@gmail.com>
2022-12-01 02:06:06 -0500

    Auger

--- gardenhose/src/Pages/Genre.js
+++ gardenhose/src/Pages/Genre.js
@@ -5,13 +5,12 @@ import {Genres} from "../Components/Genres/GenresPage";
 function Genre () {
    <Genres/>
    return (
-    <div className="flex flex-col">
-      <div className= "w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-         <h2 className="font-bold text-3x1 text-white text-left"></h2>
+    <div className="flex justify-center">
+      <div className= "w-full flex relative justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+         <h2 className="genrepick font-bold text-3x1 text-white text-left absolute flex justify-center z-30">Pick a Genre</h2>
          <select onChange={() => {}}
-         value=""
-         className ="bg-black text-white p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
-            {Genres.map((Genres)=> <option key={Genres.value}value={Genres.value}>{Genres.title}</option>)}
+          value="" className ="genre absolute flex justify-center z-30 bg-black text-white p-3 text-sm rounded-lg">
+         {Genres.map((Genres)=> <option key={Genres.value}value={Genres.value}>{Genres.title}</option>)}
          </select>
       </div>
     </div>
--- gardenhose/src/Pages/Home.js
+++ gardenhose/src/Pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Home from "../Components/Home/HomePage";
-import Test from "../Components/Home/TestApi"
 
 
 function HomePage () {
